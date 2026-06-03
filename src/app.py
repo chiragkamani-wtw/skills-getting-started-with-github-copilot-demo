ac662d0054fb585a776dd1654d23c1f9547296d9
Chirag Kamani <137502627+chiragkamani-wtw@users.noreply.github.com>
2026-06-03 09:47:50 +0530

    update available slots

--- src/app.py
+++ src/app.py
@@ -26,19 +26,19 @@ activities = {
     "Chess Club": {
         "description": "Learn strategies and compete in chess tournaments.",
         "schedule": "Fridays, 3:30 PM - 5:00 PM",
-        "max_participants": 12,
+        "max_participants": 5,
         "participants": ["michael@mergington.edu", "daniel@mergington.edu"]
     },
     "Programming Class": {
         "description": "Learn programming fundamentals and build software projects",
         "schedule": "Tuesdays and Thursdays, 3:30 PM - 4:30 PM",
-        "max_participants": 20,
+        "max_participants": 8,
         "participants": ["emma@mergington.edu", "sophia@mergington.edu"]
     },
     "Gym Class": {
         "description": "Physical education and sports activities",
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
-        "max_participants": 30,
+        "max_participants": 10,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
     }
 }
